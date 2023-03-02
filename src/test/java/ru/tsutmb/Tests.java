bafac6990e6d2bc649e99b017df99ccb2a19a621
1DhT <max.egorov01@yandex.ru>
2023-03-02 20:48:20 +0300

    Commit

---
+++ src/test/java/ru/tsutmb/Tests.java
@@ -0,0 +1,12 @@
+package ru.tsutmb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+    @Test
+    void Test1(){
+        assertEquals(true, true);
+    }
+}
