5aa30f51fe8f366c9b82b013b49df766476c196a
1DhT <max.egorov01@yandex.ru>
2023-05-13 00:25:56 +0300

    add test

--- src/main/java/ru/tsutmb/Calculate.java
+++ src/main/java/ru/tsutmb/Calculate.java
@@ -3,7 +3,7 @@ package ru.tsutmb;
 public class Calculate {
     public double Work(double x){
         if (x<=1) {
-            return Math.asin(x / (Math.pow(x, 2)));
+            return Math.asin(x / (Math.pow(x, 2) + 1));
         }
         else if (1 < x && x < 2){
             return Math.log(Math.pow(x, 4) / (1 + Math.pow(x, 4)));
--- src/test/java/ru/tsutmb/Tests.java
+++ src/test/java/ru/tsutmb/Tests.java
@@ -2,11 +2,30 @@ package ru.tsutmb;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Tests {
+    Calculate calculate = new Calculate();
     @Test
     void Test1(){
-        assertEquals(true, true);
+        double x = 1;
+        DecimalFormat df = new DecimalFormat("#.########");
+        assertEquals(df.format(this.calculate.Work(x)), "0,52359878");
+    }
+
+    @Test
+    void Test2(){
+        double x = 1.5;
+        DecimalFormat df = new DecimalFormat("#.########");
+        assertEquals(df.format(this.calculate.Work(x)), "-0,18026182");
+    }
+
+    @Test
+    void Test3(){
+        double x = 2.5;
+        DecimalFormat df = new DecimalFormat("#.########");
+        assertEquals(df.format(this.calculate.Work(x)), "-0,15566498");
     }
 }
